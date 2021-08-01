14462dcd58f530c53fca33b05dc5ff21f6fbf8a1
Lucas da Silva Postingher <lucaspostingher@outlook.com>
2021-08-01 09:58:23 -0300

    Andamento do crud de pessoas

---
+++ pessoa/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import fields, models
+from .models import Pessoa
+
+class PessoaForm(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
