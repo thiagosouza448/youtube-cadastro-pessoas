9c9351430e907a3b5e1c0b0602c85aa991ddbe55
Lucas da Silva Postingher <lucaspostingher@outlook.com>
2021-08-05 19:33:19 -0300

    Parte 6

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -3,6 +3,11 @@ from django.forms import fields, models
 from .models import Pessoa
 
 class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )
+    )
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nascimento', 'ativa']
