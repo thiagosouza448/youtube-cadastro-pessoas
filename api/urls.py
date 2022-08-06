85af284c320cab2b03b05b113bf76be23eeb254c
Lucas da Silva Postingher <lucaspostingher@outlook.com>
2022-08-06 15:08:38 -0300

    Ajuste nas urls

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,5 @@ router.register('pessoas', PessoaViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-auth/', views.obtain_auth_token)
 ]
